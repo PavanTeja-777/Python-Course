5e514d8e2670d901bd33f20eb3558266797ff18a
PavanTeja-777 <142554720+PavanTeja-777@users.noreply.github.com>
2024-06-18 12:34:24 +0530

    Add files via upload

---
+++ 10_Arithmetic-Operators/main.py
@@ -0,0 +1,7 @@
+print(10+3)  # Adiition
+print(10-3)  # Subtraction
+print(10*3)  # Multiplication
+print(10/3)  # Division
+print(10//3) # Floor Division - Ignores the Decimal portion
+print(10%3)  # Reminder(Modulus Operator)
+print(10**3) # Exponent
