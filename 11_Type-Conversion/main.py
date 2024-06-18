3afeba1b10b93bb516e4e5a9765c00ce450dccfd
PavanTeja-777 <142554720+PavanTeja-777@users.noreply.github.com>
2024-06-18 18:39:30 +0530

    Add files via upload

---
+++ 11_Type-Conversion/main.py
@@ -0,0 +1,15 @@
+a, b = "10", "11"
+print(a + b)
+
+c, d = "10", "11"
+c, d = int(c), int(d)
+print(c + d)
+
+int()    # Any datatype into int
+str()    # Any datatype into str
+float()  # Any datatype into float
+bool()   # Any datatype into bool
+list()   # Any datatype into list
+tuple()  # Any datatype into tuple
+set()    # Any datatype into set
+dict()   # Any datatype into dict
