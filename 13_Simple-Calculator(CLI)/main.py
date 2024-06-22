66ac18336a21a4f0b96b37a36a7deffe85724260
PavanTeja-777 <142554720+PavanTeja-777@users.noreply.github.com>
2024-06-22 09:46:17 +0530

    Add files via upload

---
+++ 13_Simple-Calculator(CLI)/main.py
@@ -0,0 +1,14 @@
+n1 = int(input("Enter the value of n1: "))
+n2 = int(input("Enter the value of n2: "))
+
+print("The Result of of n1 + n2 is:  ",n1+n2)
+print("The Result of of n1 - n2 is:  ",n1-n2)
+print("The Result of of n1 * n2 is:  ",n1*n2)
+print("The Result of of n1 / n2 is:  ",n1/n2)
+print("The Result of of n1 // n2 is: ",n1//n2)
+print("The Result of of n1 % n2 is:  ",n1%n2)
+print("The Result of of n1 ** n2 is: ",n1**n2)
+
+# ------------------(or)----------------------
+
+print(eval(input("Enter a Expression: ")))
---
+++ 14_Strings-1/main.py
@@ -0,0 +1,20 @@
+name = 'Follower'
+print('Hello, ' + name)
+
+a = """Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Similique quia ullam neque sed dolores. 
+Provident possimus labore vero odio voluptate"""
+print(a)
+
+lang = 'Python'
+review = 'Easy and Powerful'
+statement = f'{lang} is a {review} Language'
+print(statement)
+
+name = 'Python'
+print(name[0])
+print(name[1])
+
+name = 'Python'
+for character in name:
+    print(character)
